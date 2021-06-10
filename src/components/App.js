06fc909be2189223ca93eb4bccefae60577d02d0
Oana <oana.anghelachi@gmail.com>
2021-06-10 19:28:11 +0300

    Create button and add global styles

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Button} from './common'
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body { 
+    box-sizing: border-box; 
+    margin: 0; 
+    min-height: 100vh; 
+    font-family: 'Fira Code', monospace;
+  }
+`
+
+function App() {
+  return (
+    <>
+    <GlobalStyle />
+      <header className="App-header">
+        Header
+        <Button>Contact Us</Button>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/common/Button.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components' 
+
+const Button = styled.button` 
+    display: block;
+    width: 260px;
+    height: 70px;
+    box-shadow: none;
+    border-radius: 25px; 
+    border: none;
+    color: #fff; 
+    background: #067926;
+    font-size: 30px;
+    font-weight: 700; 
+    white-space: none;
+    cursor: pointer;
+`; 
+
+export {Button};
+
