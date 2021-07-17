66d56704a0e84e2d554d732f6ddf74e265e85a04
RiccardoBiosas <rbios@protonmail.com>
2021-07-17 19:41:07 +0200

    moved all config variables to .env and added to check to enforce them to be non-nullable

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,8 +1,22 @@
 require('dotenv').config()
 
+export const environments = {
+    PRODUCTION: 'PRODUCTION',
+    DEVELOPMENT: 'DEVELOPMENT'
+}
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID
+if(!gaTrackingId) {
+    throw new Error(`missing REACT_APP_GA_TRACKING_ID in .env`)
+}
+const environment = process.env.REACT_APP_ENVIRONMENT
+if(!environment) {
+    throw new Error(`missing REACT_APP_ENVIRONMENT in .env`)
+}
+
 const config = {
-    gaTrackingId: process.env.REACT_APP_GA_TRACKING_ID,
-    isProduction: hostname.includes('ethereumprague')
+    gaTrackingId,
+    isProduction: environment === environments.PRODUCTION
 }
 
 export default config
